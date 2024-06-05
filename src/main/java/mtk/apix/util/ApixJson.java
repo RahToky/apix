01344dae87cbad763898f1747f802b92d2fd68ab
RahToky <mahatokyrasolonirina.ext@orange.com>
2024-06-05 15:55:51 +0300

    + ApixJson.get(filename, class) to convert json to an object

--- src/main/java/mtk/apix/util/ApixJson.java
+++ src/main/java/mtk/apix/util/ApixJson.java
@@ -1,14 +1,20 @@
 package mtk.apix.util;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+
 /**
  * @author mahatoky rasolonirina
  */
 public final class ApixJson {
 
-    private ApixJson(){}
+    private ApixJson() {
+    }
 
-    public static <T> T get(String jsonFilename, Class<T> mapClass){
-        return null;
+    public static <T> T get(String filePath, Class<T> objectClass) throws IOException {
+        return new ObjectMapper().readValue(new File(filePath), objectClass);
     }
 
 }
