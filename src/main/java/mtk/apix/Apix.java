18f125fb3aba984ab32b60d0b7e42c87ca3089d7
RahToky <mahatokyrasolonirina.ext@orange.com>
2024-06-10 15:04:13 +0300

    change version in logo

--- src/main/java/mtk/apix/Apix.java
+++ src/main/java/mtk/apix/Apix.java
@@ -359,7 +359,7 @@ public class Apix {
         System.out.println("       _       ____     _   __     __                              ");
         System.out.println("      / \\     |  _ \\   | |  \\ \\   / /                              ");
         System.out.println("     / _ \\    | |_) |  | |   \\ \\ / /       0 4 / 2 0 2 4           ");
-        System.out.println("    / ___ \\   |  _ /   | |   / / \\ \\       V E R S I O N . 1.0.0   ");
+        System.out.println("    / ___ \\   |  _ /   | |   / / \\ \\       V E R S I O N . 1.0.1   ");
         System.out.println("   /_/   \\_\\  |_|      |_|  /_/   \\_\\                              \n");
     }
 
